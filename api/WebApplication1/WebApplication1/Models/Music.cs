a9c11e712457baf0b475ed4698448d465fa09f33
eyllnazzcan <eylul.naz.ozcan@gmail.com>
2022-12-13 16:01:13 +0200

    WebAPI

---
+++ api/WebApplication1/WebApplication1/Models/Music.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Music
+    {
+        public int MusicId { get; set; }
+        public string Playlist {get; set; }
+        public string AlbumName { get; set; }
+
+        public string SingerName { get; set; }
+        public string  ReleaseDate { get; set; }
+        public string  Genre { get; set; }
+    }
+}
